d6877f2ef33a11df552a7ec2c71ceccc24bf0f7d
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-03-05 13:28:39 +0100

    prueba

--- CtrolVersionesNCS/src/ProyectoJava/Mensaje.java
+++ CtrolVersionesNCS/src/ProyectoJava/Mensaje.java
@@ -3,6 +3,6 @@ package ProyectoJava;
 public class Mensaje {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
+		System.out.println("Hola mundo!,esto es una prueba");
 	}
 }
