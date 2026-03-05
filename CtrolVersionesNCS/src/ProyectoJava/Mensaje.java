e41b070999a90b0e3146c0ce6a679c44e9cc1679
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-03-05 13:48:47 +0100

    Cambio2

--- CtrolVersionesNCS/src/ProyectoJava/Mensaje.java
+++ CtrolVersionesNCS/src/ProyectoJava/Mensaje.java
@@ -4,5 +4,7 @@ public class Mensaje {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!,esto es una prueba");
+		System.out.println("Mensaje 3");
+		System.out.println("Mensaje 4");
 	}
 }
